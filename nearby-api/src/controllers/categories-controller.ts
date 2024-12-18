a324e02f926ca8aa13fb11f7c37705f0ae1c16d6
Mayderson Mello <maydersonmello@gmail.com>
2024-12-18 16:02:33 -0300

    create nearby api

---
+++ nearby-api/src/controllers/categories-controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from "express"
+import { prisma } from "@/database/prisma"
+
+class CategoriesController {
+  async index(_request: Request, response: Response, next: NextFunction) {
+    try {
+      const categories = await prisma.category.findMany({
+        orderBy: { name: "asc" },
+      })
+
+      return response.json(categories)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+
+export { CategoriesController }
